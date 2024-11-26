f2c96fd837d489e9b301d938bdad558234058195
Rajendramani <rajendramani.r@tcs.com>
2024-11-26 10:48:31 +0530

    TEST COMMIT

--- accounts/src/main/java/com/learn/accounts/dto/AccountsDTO.java
+++ accounts/src/main/java/com/learn/accounts/dto/AccountsDTO.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class AccountsDTO {
-
+    //Customize account number
     private long account_number;
     private String account_type;
     private String branch_address;  
